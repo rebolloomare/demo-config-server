ade1d86269f74166b6100de132aa9a91d5b30305
omar.rebollo <omar.rebollo@gmail.com>
2023-04-10 23:25:17 -0600

    Initial commit

---
+++ src/test/java/omare/com/mx/democonfigserver/DemoConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package omare.com.mx.democonfigserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoConfigServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
